6bcf77f690f563a2e6db74e44202b32c0fe6c7f9
Anna Horodetska and Max Reichman <max@>
2017-03-23 09:32:07 -0700

    Add CSS and JavaScript to highlight the correct rides based on height entered.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
-  var height = parseInt(prompt("Enter your height."));
-  });
+  var height = parseInt(prompt("Enter your height in inches."));
+  if (height >= 60) {
+    $(".adult").toggleClass("highlighted");
+    $(".adultkid").toggleClass("highlighted");
+  } else {
+    $(".kid").toggleClass("highlighted");
+    $(".adultkid").toggleClass("highlighted");
+  }
 });
